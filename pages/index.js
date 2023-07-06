8dce8da8925b8e526d1b2e49209110924cdf3f7a
PedroTeixeiraa <pedroteixeiraalves007@gmail.com>
2023-07-06 14:20:01 -0300

    commit inicial

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,169 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { useState } from 'react';
+import { FaPlus, FaTrash, FaCheck } from 'react-icons/fa';
 
 export default function Home() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, { text: newTask, completed: false }]);
+      setNewTask('');
+    }
+  };
+
+  const handleRemoveTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  const handleToggleTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = !updatedTasks[index].completed;
+    setTasks(updatedTasks);
+  };
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="container">
+      <h1>Task Planner</h1>
+      <div className="add-task">
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="New task"
+        />
+        <button onClick={handleAddTask}>
+          <FaPlus />
+        </button>
+      </div>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
+      <ul className="task-list">
+        {tasks.map((task, index) => (
+          <li key={index} className={`task-item ${task.completed ? 'completed' : ''}`}>
+            <span
+              className="task-text"
+            >
+              {task.text}
+            </span>
+            <div className="task-actions">
+              <button
+                className="action-button"
+                onClick={() => handleToggleTask(index)}
+              >
+                {task.completed ? <FaCheck /> : <FaCheck className="hidden" />}
+              </button>
+              <button
+                className="action-button"
+                onClick={() => handleRemoveTask(index)}
+              >
+                <FaTrash />
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
 
       <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
+          .pagina {
+            display: flex;
+            flex-direction: column;
+            align-items: center; 
+            height: 100vh;
+            width: 100vw;
+            max-width: 400px;
+          }
+          .container {
+            border: 2px solid #ccc;
+            border-radius: 8px;
+            padding: 16px;
+            margin: 16px;
+            max-width: 400px;
+          }
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
+          .add-task {
+            display: flex;
+            align-items: center;
+            margin-bottom: 16px;
+          }
+
+          .add-task input {
+            margin-right: 8px;
+            padding: 4px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            font-size: 14px;
+            width: 200px;
+          }
+
+          .add-task button {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: 4px;
+            border: none;
+            background-color: #0070f3;
+            color: #fff;
+            border-radius: 4px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+          }
+
+          .add-task button:hover {
+            background-color: #0053b3;
+          }
+
+          .task-list {
+            list-style-type: none;
+            padding: 0;
+            margin: 0;
+          }
+
+          .task-item {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            border-bottom: 1px solid #ccc;
+            padding: 8px 0;
+              }
+
+          .task-item.completed {
+            text-decoration: line-through;
+            color: #aaa;
+          }
+
+          .task-text {
+            flex: 1;
+            margin-right: 8px;
+            font-size: 14px;
+            cursor: pointer;
+          }
+
+          .task-actions {
+            display: flex;
+            gap: 8px;
+          }
+
+          .action-button {
+            padding: 4px;
+            border: none;
+            background-color: #ff0000;
+            color: #fff;
+            border-radius: 4px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+          }
+
+          .action-button:hover {
+            background-color: #cc0000;
+          }
+
+          .hidden {
+            display: none;
+          }
       `}</style>
     </div>
-  )
+  );
 }
