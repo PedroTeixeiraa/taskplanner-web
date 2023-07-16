b53582e9d9e8c8365c19d3e9eb058258b1d4e978
PedroTeixeiraa <pedroteixeiraalves007@gmail.com>
2023-07-16 18:10:56 -0300

    update react project

---
+++ src/App.jsx
@@ -0,0 +1,102 @@
+import { useEffect, useState } from 'react';
+import { FaPlus, FaTrash, FaCheck } from 'react-icons/fa';
+import { useRequisicoes } from './use-requisicoes';
+
+import './App.css';
+
+export default function App() {
+  const [tasks, setTasks] = useState();
+  const [newTask, setNewTask] = useState('');
+
+  const { salvarTask, buscarTasks, atualizar, remover } = useRequisicoes()
+
+  useEffect(() => {
+    const buscar = async () => {
+      const tasks = await buscarTasks()
+      setTasks(tasks)
+    }
+
+    if (!tasks) {
+      buscar()
+    }
+  })
+
+  const handleAddTask = async () => {
+    if (newTask.trim() !== '') {
+      const dados = {
+        nome: newTask,
+        concluida: 0,
+      }
+      await salvarTask(dados).then(() => {
+        setTasks([...tasks, { nome: newTask, concluida: false }]);
+        setNewTask('');
+      })
+    }
+  };
+
+  const handleRemoveTask = async (index, id) => {
+    await remover(id).then(() => {
+      const updatedTasks = [...tasks]
+      updatedTasks.splice(index, 1)
+      setTasks(updatedTasks)
+    })
+  }
+
+  const handleToggleTask = async (index, task) => {
+    const updatedTasks = [...tasks]
+    const dados = {
+      nome: task.id,
+      concluida: !updatedTasks[index].concluida ? 1 : 0
+    }
+
+    await atualizar(dados, task.id).then(() => {
+      updatedTasks[index].concluida = !updatedTasks[index].concluida
+      setTasks(updatedTasks)
+    })
+  }
+
+  return (
+    <div className="pagina">
+      <div className="container">
+        <h1>Task Planner</h1>
+        <div className="add-task">
+          <input
+            type="text"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            placeholder="New task"
+          />
+          <button onClick={handleAddTask}>
+            <FaPlus />
+          </button>
+        </div>
+
+        <ul className="task-list">
+          {tasks?.map((task, index) => (
+            <li key={index} className={`task-item ${task.concluida ? 'completed' : ''}`}>
+              <span
+                className="task-text"
+              >
+                {task.nome}
+              </span>
+              <div className="task-actions">
+                <button
+                  className="action-button"
+                  onClick={() => handleToggleTask(index, task)}
+                >
+                  {task.concluida ? <FaCheck /> : <FaCheck className="hidden" />}
+                </button>
+                <button
+                  className="action-button"
+                  onClick={() => handleRemoveTask(index, task.id)}
+                >
+                  <FaTrash />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
--- src/use-requisicoes.js
+++ src/use-requisicoes.js
@@ -1,13 +1,17 @@
+// eslint-disable-next-line react-hooks/exhaustive-deps
 import axios from "axios"
 import { useMemo } from "react"
 
-const useRequisicoes = urlBase => {
+const useRequisicoes = () => {
 
-    const urlCompleta = `${urlBase}/tasks`
+    const URL_BASE = process.env.REACT_APP_URL_BASE
+    const urlCompleta = `${URL_BASE}/tasks`
 
     const salvarTask = dados => axios.post(urlCompleta, dados)
 
-    const buscarTasks = async () => axios.get(urlCompleta).then(r => r.data)
+    const buscarTasks = async () => await axios.get(urlCompleta)
+        .then(r => r.data)
+        .catch(r => [])
 
     const atualizar = (dados, idTask) => axios.put(`${urlCompleta}/${idTask}`, dados)
 
